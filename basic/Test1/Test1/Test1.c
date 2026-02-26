3fd1e67875f7dcf9139a56c35030c2a14cc4822c
chen hyenbeen <123910896+gusqls7748@users.noreply.github.com>
2026-02-26 16:05:18 +0900

    커밋

---
+++ basic/Test1/Test1/Test1.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main() {
+
+	char arr[4] = "hong";
+
+	//printf("arr : %c", arr);
+	printf("arr[0]: %c\n", arr[0]);
+	printf("arr[1]: %c\n", arr[1]);
+	printf("arr[2]: %c\n", arr[2]);
+	printf("arr[3]: %c\n", arr[3]);
+	printf("arr[4]: %c\n", arr[4]); // 이상한 문장이 나옴
+
+
+	return 0;
+}
