8b4725b2dbe6ccabee4fa281424473645745af4d
chen hyenbeen <123910896+gusqls7748@users.noreply.github.com>
2026-02-26 16:33:34 +0900

    커밋

--- basic/Test1/Test1/Test1.c
+++ basic/Test1/Test1/Test1.c
@@ -1,16 +1,23 @@
 ﻿#include <stdio.h>
 
+int n;
+
 int main() {
 
-	char arr[4] = "hong";
+	char arr[] = "hong";
+	char *arr1 = "hong";
 
 	//printf("arr : %c", arr);
-	printf("arr[0]: %c\n", arr[0]);
-	printf("arr[1]: %c\n", arr[1]);
-	printf("arr[2]: %c\n", arr[2]);
-	printf("arr[3]: %c\n", arr[3]);
-	printf("arr[4]: %c\n", arr[4]); // 이상한 문장이 나옴
+	//printf("arr[0]: %c\n", arr[0]);
+	//printf("arr[1]: %c\n", arr[1]);
+	//printf("arr[2]: %c\n", arr[2]);
+	//printf("arr[3]: %c\n", arr[3]);
+	//printf("arr[4]: %s\n", &arr[4]); // 이상한 문장이 나옴
 
+	printf("arr: %p\n", arr);
+	printf("arr1: %p\n", &arr1);
+	printf("hong: %p\n", *arr1);
+	printf("n: %p\n", &n);
 
 	return 0;
 }
